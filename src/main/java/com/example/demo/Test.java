acd6507d7227aa9edc082a68cd0c5f1a678de75f
SinghParam002 <nayakprince765@gmail.com>
2024-09-20 16:07:42 +0530

    Adding test class

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class Test {
+    int a;
+    int b;
+
+    Test(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Test [a=" + a + ", b=" + b + "]";
+    }
+
+}
